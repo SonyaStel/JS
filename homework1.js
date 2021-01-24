0359a3e3d386d9eeb2d94bb382bb6009ac4aa8ae
SonyaStel <77762257+SonyaStel@users.noreply.github.com>
2021-01-24 08:06:43 +0200

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,56 @@
+//task 2
+alert("Stelmah");
+//task 3
+var data = 31;
+var note = "Some text";
+console.log(data);
+console.log(note);
+note = data;
+console.log(data);
+console.log(note);
+// task 4
+var myCar = {
+    make: "Mazda",
+    model: "RX5" ,
+    year: 2015,
+}
+var myCarColor;
+var mileage = null;
+
+
+// task 5
+let isAdult = confirm ("Are you an adult?");
+alert (isAdult);
+
+//task 6
+var myName = "Sofia";
+var myLastName = "Stelmah";
+var myGroup = "569 JS Core";
+var myYearOfBirth = 1996;
+var notMarried = true;
+console.log(typeof myYearOfBirth);
+console.log(typeof notMarried);
+console.log(typeof myName);
+
+var x
+var age = 15
+age = null 
+console.log(typeof age);
+console.log(typeof x);
+
+
+
+
+//task 7
+let login = prompt ("What is your login?");
+let isEmail = prompt ("What is your email?");
+let isPassword = prompt ("What is your password?");
+alert("Dear " + login , "your email is" +isEmail , ", your password is" + isPassword);
+
+
+//task 8
+alert(60 * 60); //кількість секунд в годині
+alert(60 * 60 * 24) //кількість секунд в добі
+alert(60 * 60 * 24 * 30) //кількість секунд в місяці 
+
+
